4a9004acba242bc2fe8161cc721417033894d2cf
izbz wh <731215820@qq.com>
2019-04-26 16:33:43 +0800

    feat(GridToolBar): 增加GridToolBar功能

--- build/index.js
+++ build/index.js
@@ -1,14 +1,19 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.GridToolBar = undefined;
 
-var _ComplexGrid = require('./ComplexGrid');
+var _ComplexGrid = require("./ComplexGrid");
 
 var _ComplexGrid2 = _interopRequireDefault(_ComplexGrid);
 
+var _ToolBar = require("./ToolBar");
+
+var _ToolBar2 = _interopRequireDefault(_ToolBar);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+exports.GridToolBar = _ToolBar2["default"];
 exports["default"] = _ComplexGrid2["default"];
-module.exports = exports['default'];
